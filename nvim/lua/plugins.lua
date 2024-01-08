9f51abd6a684e18aed2c0ec1578cc1456ea6f6d9
secwall <secwall@secwall.me>
2024-01-08 20:28:40 +0300

    Properly configure rustaceanvim

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -277,6 +277,35 @@ return {
         'mrcjkb/rustaceanvim',
         version = '^3',
         ft = { 'rust' },
+        opts = {
+            server = {
+                settings = {
+                    ["rust-analyzer"] = {
+                        cargo = {
+                            allFeatures = true,
+                            loadOutDirsFromCheck = true,
+                            runBuildScripts = true,
+                        },
+                        checkOnSave = {
+                            allFeatures = true,
+                            command = "clippy",
+                            extraArgs = { "--no-deps" },
+                        },
+                        procMacro = {
+                            enable = true,
+                            ignored = {
+                                ["async-trait"] = { "async_trait" },
+                                ["napi-derive"] = { "napi" },
+                                ["async-recursion"] = { "async_recursion" },
+                            },
+                        },
+                    },
+                },
+            },
+        },
+        config = function(_, opts)
+            vim.g.rustaceanvim = vim.tbl_deep_extend("force", {}, opts or {})
+        end,
     },
     {
         'lvimuser/lsp-inlayhints.nvim',
